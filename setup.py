aad206982a607e44eef812725f875ddeaf8cc8e9
y1zhou <zhou.zy.yi@gmail.com>
2021-10-28 00:00:27 -0400

    feat(brenda): more of the parser implemented

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="met_path_finder",
+    verion="0.0.1",
+    author="Yi Zhou",
+    url="https://github.com/y1zhou/met_path_finder",
+    packages=find_packages(),
+    keywords=["bioinformatics", "metabolic-pathway", "graph"],
+    python_requires=">=3.6",
+    # install_requires=["pandas", "numpy"],
+)
