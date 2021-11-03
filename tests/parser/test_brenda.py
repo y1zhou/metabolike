efee1a1fc272463762e1f7bbfe4887403a5748d2
y1zhou <zhou.zy.yi@gmail.com>
2021-11-03 18:09:12 -0400

    test(brenda): tests for generic and reaction BRENDA parsers

--- tests/parser/test_brenda.py
+++ tests/parser/test_brenda.py
@@ -1,11 +1,56 @@
+from pathlib import Path
+
 import pandas as pd
 import pandas.testing as pdt
-from met_path_finder.parser.brenda import read_brenda
+import pytest
+from met_path_finder.parser.brenda import Brenda
 
 
-def test_brenda_reader():
-    df = read_brenda("tests/data/brenda_test.txt", clean=True)
+@pytest.fixture
+def brenda_data():
+    br = Brenda()
+    br.df = br.read_brenda(Path("tests/data/brenda_test.txt"))
+    return br
 
-    assert type(df) == pd.DataFrame
-    assert df.shape == (57, 3)
-    pdt.assert_index_equal(df.columns, pd.Index(["ID", "field", "description"]))
+
+def test_read_brenda(brenda_data):
+    # Test that the dataframe is read correctly
+    assert isinstance(brenda_data.df, pd.DataFrame)
+    assert brenda_data.df.shape == (57, 3)
+    pdt.assert_index_equal(
+        brenda_data.df.columns, pd.Index(["ID", "field", "description"])
+    )
+
+    # DataFrame columns should match the expected values
+    assert brenda_data.df["ID"].str.match(r"^(\d+\.){3}\d+$").all()
+    assert brenda_data.df["field"].str.match(r"^[A-Z_05]+$").all()
+
+
+def test_text_to_tree_generic(brenda_data: Brenda):
+    """Test the generic grammar."""
+    df = brenda_data.df
+    text = (
+        df.query("ID == '1.1.1.1' & field == 'PROTEIN'")
+        .filter(["description"])
+        .values[0, 0]
+    )
+
+    tree = brenda_data._text_to_tree(text, "PROTEIN")
+    assert isinstance(tree, list)
+    assert len(tree) == 164
+    assert set(tree[0].keys()) - set(["protein_id", "description", "ref_id"]) == set()
+    assert isinstance(tree[0]["protein_id"], list)
+    assert isinstance(tree[0]["protein_id"][0], int)
+
+
+def test_text_to_tree_reaction(brenda_data: Brenda):
+    """Test the reaction grammar."""
+    df = brenda_data.df
+    text = (
+        df.query("ID == '1.1.1.1' & field == 'SUBSTRATE_PRODUCT'")
+        .filter(["description"])
+        .values[0, 0]
+    )
+
+    tree = brenda_data._text_to_tree(text, "SUBSTRATE_PRODUCT")
+    assert isinstance(tree, list)
