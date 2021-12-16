7a83081b7b4c871f9f3fc4b323253fc23107df27
y1zhou <zhou.zy.yi@gmail.com>
2021-12-15 21:56:42 -0500

    feat(metacyc): parse compartments and species

--- metabolike/parser/metacyc.py
+++ metabolike/parser/metacyc.py
@@ -7,28 +7,41 @@ from metabolike import db
 
 logger = logging.getLogger(__name__)
 
+NODE_LABELS = ["Pathway", "Compartment", "Reaction", "Compound", "GeneProduct"]
+
 
 class Metacyc:
+    """
+    Converting MetaCyc files to a Neo4j database.
+    Documentation on the MetaCyc files and ofrmat FAQs can be found at:
+
+    - MetaCyc data files download: https://metacyc.org/downloads.shtml
+    - MetaCyc file formats: http://bioinformatics.ai.sri.com/ptools/flatfile-format.html
+    - SBML FAQ: https://synonym.caltech.edu/documents/faq
+    """
+
     def __init__(self, filepath: Union[str, Path], neo4j_driver: db.Neo4jDriver):
         self.neo4j_driver = neo4j_driver
-
-        # Read SBML file
         self.filepath = Path(filepath).expanduser().resolve()
+
+    def setup(self, force: bool = False):
+        # Read SBML file
         self.doc = self.read_sbml()
         self.model: libsbml.Model = self.doc.getModel()
         self.db_name: str = self.model.getId().lower()
 
         # Setup Neo4j database
-        self.setup_neo4j()
+        self.setup_graph_db(force=force)
 
     def read_sbml(self) -> libsbml.SBMLDocument:
         reader = libsbml.SBMLReader()
-        metacyc = reader.readSBML(self.filepath)
+        metacyc = reader.readSBMLFromFile(self.filepath)
+        logger.info("Finished reading SBML file")
 
         for i in range(metacyc.getNumErrors()):
             err = metacyc.getError(i)
             logger.warning(
-                "SBML reader raised %s at line %d, column %d: %s",
+                "SBML reader raised %s during parsing at line %d, column %d: %s",
                 err.getSeverityAsString(),
                 err.getLine(),
                 err.getColumn(),
@@ -37,7 +50,21 @@ class Metacyc:
 
         return metacyc
 
-    def setup_neo4j(self, **kwargs):
+    def setup_graph_db(self, **kwargs):
+        """
+        Create Neo4j database and set proper constraints. `Reaction` nodes are
+        central in the schema:
+
+        ```
+                     Pathway
+                        │
+        Compartment───Reaction───Compound
+            │           │          │
+            └───────────┼──────────┘
+                        │
+                    GeneProduct
+        ```
+        """
         # Create database
         try:
             db.create(self.neo4j_driver, self.db_name, **kwargs)
@@ -57,7 +84,7 @@ class Metacyc:
 
             # Constraints automatically create indexes, so we don't need to
             # create them manually.
-            for label in ["Pathway", "Compartment", "Reaction", "Species"]:
+            for label in NODE_LABELS:
                 logger.debug(f"Creating constraint for {label} nodes")
                 r = session.run(
                     f"""CREATE CONSTRAINT IF NOT EXISTS
@@ -67,3 +94,54 @@ class Metacyc:
                     logger.warning(
                         f"Could not create constraint for {label} nodes: {r}"
                     )
+
+    def sbml_to_graph(self):
+        """
+        Populate Neo4j database with SBML data.
+
+        Nodes are created for each SBML element using `MERGE` statements:
+        https://neo4j.com/docs/cypher-manual/current/clauses/merge/#merge-merge-with-on-create
+        """
+        with self.neo4j_driver.session(database=self.db_name) as session:
+            # Compartments
+            for c in self.model.getListOfCompartments():
+                c: libsbml.Compartment
+                session.write_transaction(
+                    lambda tx: tx.run(
+                        """
+                        MERGE (c:Compartment {mcId: $mcId})
+                        ON CREATE
+                        SET c.displayName = $name;
+                        """,
+                    ),
+                    mcId=c.getId(),
+                    name=c.getName(),
+                )
+
+            # Compounds, i.e. metabolites, species
+            for s in self.model.getListOfSpecies():
+                s: libsbml.Species
+                # Basic properties
+                session.write_transaction(
+                    lambda tx: tx.run(
+                        """
+                        MATCH (cpt:Compartment {mcId: $compartment})
+                        MERGE (c:Compound {mcId: $mcId})-[:hasCompartment]->(cpt)
+                        ON CREATE
+                            SET c.displayName = $name,
+                                c.charge = $charge,
+                                c.chemicalFormula = $formula,
+                                c.boundaryCondition = $boundaryCondition,
+                                c.hasOnlySubstanceUnits = $hasOnlySubstanceUnits,
+                                c.constant = $constant;
+                        """,
+                        mcId=s.getId(),
+                        name=s.getName(),
+                        compartment=s.getCompartment(),
+                        charge=s.getCharge(),
+                        formula=s.getPlugin("fbc").getChemicalFormula(),
+                        boundaryCondition=s.getBoundaryCondition(),  # unchanged by reactions
+                        hasOnlySubstanceUnits=s.getHasOnlySubstanceUnits(),
+                        constant=s.getConstant(),
+                    )
+                )
