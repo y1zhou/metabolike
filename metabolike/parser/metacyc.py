035232357f1ea448d9f3c9a2e89b21df0e3022ec
y1zhou <zhou.zy.yi@gmail.com>
2021-12-17 13:22:11 -0500

    feat(metacyc): add RDF nodes in the Annotation section

--- metabolike/parser/metacyc.py
+++ metabolike/parser/metacyc.py
@@ -1,6 +1,6 @@
 import logging
 from pathlib import Path
-from typing import Union
+from typing import Dict, Tuple, Union
 
 import libsbml
 from metabolike import db
@@ -181,3 +181,55 @@ class Metacyc:
                 # the model component to annotate (subject), the relationship
                 # between the model component and the annotation (predicate),
                 # and a term describing the component (object).
+                logger.debug(f"Adding RDF nodes for Compound {mcid}")
+                cvterms = s.getCVTerms()
+                for cvterm in cvterms:
+                    # Get the biological qualifier type of the terms
+                    cvterm: libsbml.CVTerm
+                    bio_qual = BIO_QUALIFIERS[cvterm.getBiologicalQualifierType()]
+                    # Get the content of each RDF term
+                    uris = [
+                        self.split_uri(cvterm.getResourceURI(i))
+                        for i in range(cvterm.getNumResources())
+                    ]
+                    uris = {x[0]: x[1] for x in uris}
+                    # Generate the Cypher statements for each RDF term
+                    uri_cypher = self.generate_cypher_for_uri(uris)
+
+                    session.write_transaction(
+                        lambda tx: tx.run(
+                            f"""
+                            MATCH (c:Compound {{mcId: $mcId}})
+                            MERGE (n:RDF)<-[:{bio_qual}]-(c)
+                            ON CREATE
+                                SET {uri_cypher};
+                            """,
+                            mcId=mcid,
+                            **uris,
+                        )
+                    )
+
+    @staticmethod
+    def split_uri(uri: str) -> Tuple[str, str]:
+        """Split a URI into a namespace and an annotation term.
+
+        Args:
+            uri: URI to split.
+
+        Returns:
+            Tuple of namespace and annotation term.
+        """
+        # First three elements are from http://identifiers.org/
+        res = uri.split("/")[3:]
+        resource, identifier = res[0], res[1:]
+        resource = "".join(x.capitalize() for x in resource.split("."))
+        identifier = "".join(identifier)
+        return resource, identifier
+
+    @staticmethod
+    def generate_cypher_for_uri(uris: Dict[str, str]) -> str:
+        cypher = []
+        for label in uris.keys():
+            cypher.append(f"n.{label} = ${label}")
+
+        return ",".join(cypher)
