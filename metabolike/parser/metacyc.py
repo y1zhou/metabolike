5721188c179ec83756118e885287e70d75c39710
y1zhou <zhou.zy.yi@gmail.com>
2022-01-18 15:16:39 -0500

    refactor(metacyc): extract helper functions for converting camelcase and constructing dict

--- metabolike/parser/metacyc.py
+++ metabolike/parser/metacyc.py
@@ -752,3 +752,46 @@ class Metacyc:
                 props[f] = enum_pattern.sub("", props[f])
 
         return props
+
+
+def _snake_to_camel(s: str, sep: str = "-") -> str:
+    """Convert snake_case to CamelCase.
+
+    Args:
+        s: String to convert.
+        sep: Separator to use.
+
+    Returns:
+        camelCase string.
+    """
+    s_camel = [x.capitalize() for x in s.split(sep)]
+    s_camel[0] = s_camel[0].lower()
+    return "".join(s_camel)
+
+
+def _add_kv_to_dict(
+    d: Dict[str, Any], k: str, v: Any, as_list: bool = False
+) -> Dict[str, Any]:
+    """Add a key-value pair to a dictionary.
+
+    Args:
+        d: Dictionary to add the key-value pair to.
+        k: Key to add.
+        v: Value to add.
+
+    Returns:
+        Dictionary with the key-value pair added. If the key already exists,
+        the value is saved as a list.
+    """
+    k_camel = _snake_to_camel(k)
+    if as_list:
+        if k_camel in d:
+            d[k_camel].append(v)
+        else:
+            d[k_camel] = [v]
+    else:
+        d[k_camel] = v
+
+    return d
+
+
