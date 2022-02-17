c212ead9f935d4cb9172508d49f399992974070c
y1zhou <zhou.zy.yi@gmail.com>
2022-02-17 17:33:33 -0500

    feat(brenda): read and clean the BKMS-react table

--- metabolike/parser/bkms_react.py
+++ metabolike/parser/bkms_react.py
@@ -1,4 +1,6 @@
 import tarfile
+from io import StringIO
+from pathlib import Path
 
 import pandas as pd
 import requests
@@ -40,3 +42,55 @@ def _extract_bkms_tarball(filepath: str) -> None:
     tar.extractall()
     tar.close()
 
+
+def read_bkms(filepath: str, clean: bool = True) -> pd.DataFrame:
+    """
+    Read the BKMS-react table and prepare it for further processing.
+
+    The table contains random ``^M`` characters in some rows. These characters
+    won't break pandas, but they will make the parsed table unexpectedly long.
+    The ``clean`` parameter can be used to remove these characters.
+
+    BRENDA takes EC numbers as identifiers, so we need entries with non-empty
+    ``EC_Number`` and ``Reaction_ID_MetaCyc`` columns. There is a
+    ``Reaction_ID_BRENDA`` column that's sometimes non-empty when the EC number
+    is missing, but the field is not documented and it's not clear how it can be
+    mapped to an entry in the BRENDA text file.
+
+    To be extra conservative, we only keep entries with non-empty
+    ``Reaction_ID_KEGG`` columns. Only reactions in MetaCyc/BioCyc that are
+    associated with matching EC numbers *and* KEGG IDs will be annotated.
+
+    Args:
+        filepath: The path to the BKMS-react ``.tsv`` file.
+        clean: Remove the``^M`` characters.
+
+    Returns:
+        A pandas dataframe:
+    """
+    fp = Path(filepath).expanduser().resolve()
+    if not clean:
+        df = pd.read_table(fp, sep="\t")
+        return df.drop_duplicates()
+
+    with open(fp, "r") as f:
+        lines = f.readlines()
+    lines = [line.replace(r"\r", "") for line in lines]
+    df: pd.DataFrame = pd.read_table(StringIO("".join(lines)), sep="\t")
+    df = (
+        df.filter(
+            [
+                "EC_Number",
+                "Reaction_ID_KEGG",
+                "Reaction_ID_MetaCyc",
+                "MetaCyc_Pathway_ID",
+                "Reaction",
+            ]
+        )
+        .query("Reaction_ID_MetaCyc.notna() & Reaction_ID_KEGG.notna()")
+        .drop_duplicates()
+    )
+    # TODO: what are the BRENDA reaction IDs? Can we map them to MetaCyc?
+    # TODO: Can the MetaCyc IDs be mapped to organism-specific BioCyc IDs?
+    # TODO: clean up the EC numbers (non-canonical, missing values, etc.)
+    return df
