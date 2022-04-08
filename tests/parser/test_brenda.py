5c72a95bf0c584c7b9df2e2a4eb73a1cf9359b93
y1zhou <zhou.zy.yi@gmail.com>
2022-04-08 16:09:13 -0400

    test(brenda): brenda file parsing and result caching

--- tests/parser/test_brenda.py
+++ tests/parser/test_brenda.py
@@ -3,14 +3,46 @@ from pathlib import Path
 import pandas as pd
 import pandas.testing as pdt
 import pytest
-from metabolike.parser.brenda import (_get_parser_from_field, _read_brenda,
-                                      _text_to_tree)
+from metabolike.parser.brenda import (
+    _get_parser_from_field,
+    _read_brenda,
+    _text_to_tree,
+    parse_brenda,
+)
+
+brenda_txt = Path("tests/data/brenda_test.txt")
+brenda_cache = brenda_txt.with_suffix(".csv")
+brenda_parsed = brenda_txt.with_suffix(".json")
 
 
 @pytest.fixture
 def brenda_data():
-    df = _read_brenda(Path("tests/data/brenda_test.txt"))
-    return df
+    return _read_brenda(brenda_txt)
+
+
+def test_read_brenda_cache():
+    with pytest.raises(ValueError):
+        _read_brenda(Path("tests/data/fake_file.txt"))
+
+    df = _read_brenda(brenda_txt, cache=True)
+    assert brenda_cache.is_file()
+
+    df2 = _read_brenda(brenda_txt, cache=True)
+    pdt.assert_frame_equal(df, df2)
+
+    brenda_cache.unlink()
+
+
+def test_parse_brenda():
+    ec = ["1.1.1.1", "6.3.5.8"]
+    d1 = parse_brenda(brenda_txt, cache=True, ec_nums=ec)
+    assert brenda_parsed.is_file()
+
+    d2 = parse_brenda(brenda_txt, cache=True, ec_nums=ec)
+    assert d1 == d2
+
+    brenda_cache.unlink()
+    brenda_parsed.unlink()
 
 
 def test_read_brenda(brenda_data):
