55a24318fbc121c4cff7154cc24569ff936de616
y1zhou <zhou.zy.yi@gmail.com>
2022-07-02 15:10:40 -0400

    feat(algo): map gene expression to graph

--- metabolike/algo/omics.py
+++ metabolike/algo/omics.py
@@ -1,5 +1,6 @@
-from typing import Set
+from typing import Dict, Set
 
+import pandas as pd
 from metabolike.db import Neo4jClient
 
 
@@ -13,3 +14,145 @@ def get_all_gene_products(db: Neo4jClient) -> Set[str]:
         """
     )
     return {n["ec"] for n in ec}
+
+
+def get_table_of_gene_products(db: Neo4jClient, rdf_fields: Dict[str, str] = None):
+    """
+    Retrieves all reaction-associated gene products.
+    Args:
+        db: A Neo4j client connected to the graph database.
+        rdf_fields: Properties of the RDF nodes, and the desired output name.
+          For example, {"ncbigene": "entrez"} would extract the ``ncbigene``
+          property from the RDF nodes and output it in the ``entrez`` column.
+
+    Returns:
+        A list of entries with the reaction ID, the metaId of the gene, the
+          gene symbol, and other fields from RDF nodes whenever available.
+    """
+    query = """
+        MATCH (r:Reaction)-[:hasGeneProduct|hasMember|hasComponent*]->(gp:GeneProduct)
+        WHERE NOT gp:GeneProductSet OR gp:GeneProductComplex
+        WITH r.metaId AS rxn_id, gp
+        OPTIONAL MATCH (gp)-[:hasRDF {bioQualifier: 'isEncodedBy'}]->(rdf:RDF)
+        RETURN rxn_id, gp.metaId AS gene_id, gp.name AS gene
+    """
+    if rdf_fields:
+        for field, col in rdf_fields.items():
+            query += f", rdf.{field} AS {col}"
+    return db.read(query)
+
+
+class ReactionGeneMap:
+    """
+
+    Attributes:
+        database_connection: Connection to the Neo4j database.
+        rxn_exp: expression levels of each reaction.
+        expression_levels: gene expression data.
+        gene_groups: parent-child mapping of gene product sets and complexes.
+        gene_id_map: mapping between gene metaId and gene name.
+    """
+
+    def __init__(
+        self,
+        database_connection: Neo4jClient,
+        expression_levels: dict[str, float],
+        gene_groups: pd.DataFrame | None = None,
+        gene_id_map: pd.DataFrame | None = None,
+        gene_set_reduce_func: callable = max,
+        gene_complex_reduce_func: callable = min,
+    ):
+
+        self.db = database_connection
+        self.gene_exp = expression_levels
+        self.rxn_exp: dict[str, float] = {}
+
+        if gene_groups is None:
+            self.gene_groups = self._get_gene_groups()
+        else:
+            self.gene_groups = gene_groups
+
+        if gene_id_map is None:
+            self.gene_id_map = self._get_gene_id_map()
+        else:
+            self.gene_id_map = gene_id_map
+
+        self._valid_gene_groups = set(self.gene_groups["group_id"].unique())
+        self._set_func = gene_set_reduce_func
+        self._complex_func = gene_complex_reduce_func
+
+        # Setup reaction expression levels
+        rxn_genes = self._get_top_level_rxn_gene_mapping()
+
+        for rxn, gene in rxn_genes.items():
+            if gene_name := self.gene_id_map.get(gene):  # not None or ""
+                self.rxn_exp[rxn] = self.get_gene_expression(gene_name)
+
+    def get_gene_expression(self, gene_name: str):
+        if (gene_exp := self.gene_exp.get(gene_name)) is not None:
+            return gene_exp
+        elif gene_name in self._valid_gene_groups:
+            return self.calc_reaction_gene_group_expression(gene_name)
+        else:
+            # raise ValueError(f"Unknown gene product {gene_name}")
+            print(gene_name)
+
+    def calc_reaction_gene_group_expression(self, gene_group_id: str):
+        """
+        Calculate the expression level of a given gene product set or
+        complex node.
+        """
+        genes = self.gene_groups.loc[self.gene_groups["group_id"] == gene_group_id, :]
+        gene_ids = genes["members"].values
+        group_type = genes["group_type"].values[0]
+
+        res = [
+            self.get_gene_expression(gene_name)
+            for gene_id in gene_ids
+            if (gene_name := self.gene_id_map.get(gene_id))
+        ]
+
+        if group_type == "Set":
+            return self._set_func(res)
+        elif group_type == "Complex":
+            return self._complex_func(res)
+        else:
+            raise ValueError(f"Unknown gene group type: {group_type}")
+
+    def _get_top_level_rxn_gene_mapping(self):
+        """
+        Get unique reaction -> gene product mappings. The "gene" column could
+        be GeneProduct, GeneProductSet, or GeneProductComplex nodes.
+        """
+        rxn2gene = self.db.read(
+            """
+            MATCH (r:Reaction)-[:hasGeneProduct]->(g)
+            WHERE g:GeneProduct OR g:GeneProductSet OR g:GeneProductComplex
+            RETURN r.metaId AS rxn, g.metaId AS gene_id;
+            """
+        )
+        res = {n["rxn"]: n["gene_id"] for n in rxn2gene}
+        return res
+
+    def _get_gene_groups(self):
+        group2child = self.db.read(
+            """
+            MATCH (gps:GeneProductSet)-[:hasMember]->(gp)
+            RETURN gps.metaId AS group_id, "Set" AS group_type, COLLECT(gp.metaId) AS members
+            UNION ALL
+            MATCH (gpc:GeneProductComplex)-[:hasComponent]->(gp)
+            RETURN gpc.metaId AS group_id, "Complex" AS group_type, COLLECT(gp.metaId) AS members
+            """
+        )
+        res = pd.DataFrame([x for x in group2child])
+        return res.explode("members").reset_index(drop=True)
+
+    def _get_gene_id_map(self):
+        genes = self.db.read(
+            """
+            MATCH (gp:GeneProduct)
+            WHERE NOT (gp:GeneProductSet OR gp:GeneProductComplex)
+            RETURN gp.metaId AS gene_id, gp.name AS gene_name;
+            """
+        )
+        return {n["gene_id"]: n["gene_name"] for n in genes}
--- metabolike/algo/routes.py
+++ metabolike/algo/routes.py
@@ -2,6 +2,7 @@ from typing import Dict, Iterable, List, Set, Tuple
 
 import numpy as np
 import pandas as pd
+from metabolike.algo.omics import get_table_of_gene_products
 from metabolike.db import Neo4jClient
 from metabolike.utils import generate_gene_reaction_rule
 
@@ -321,32 +322,6 @@ def get_reaction_route_between_compounds(
         num_routes=num_routes,
         max_hops=max_hops * 2,
     )
-
-
-def get_table_of_gene_products(db: Neo4jClient, rdf_fields: Dict[str, str] = None):
-    """
-    Retrieves all reaction-associated gene products.
-    Args:
-        db: A Neo4j client connected to the graph database.
-        rdf_fields: Properties of the RDF nodes, and the desired output name.
-          For example, {"ncbigene": "entrez"} would extract the ``ncbigene``
-          property from the RDF nodes and output it in the ``entrez`` column.
-
-    Returns:
-        A list of entries with the reaction ID, the metaId of the gene, the
-        gene symbol, and other fields from RDF nodes whenever available.
-    """
-    query = """
-        MATCH (r:Reaction)-[:hasGeneProduct|hasMember|hasComponent*]->(gp:GeneProduct)
-        WHERE NOT gp:GeneProductSet OR gp:GeneProductComplex
-        WITH r.metaId AS rxn_id, gp
-        OPTIONAL MATCH (gp)-[:hasRDF {bioQualifier: 'isEncodedBy'}]->(rdf:RDF)
-        RETURN rxn_id, gp.metaId AS gene_id, gp.name AS gene
-    """
-    if rdf_fields:
-        for field, col in rdf_fields.items():
-            query += f", rdf.{field} AS {col}"
-    return db.read(query)
 
 
 def find_compound_outflux_routes(
